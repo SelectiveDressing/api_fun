c1d133ee86abe7a6f0d4b8121d339a9685293428
billbunkum <william.richard.hurst@gmail.com>
2016-09-09 14:17:41 -0400

    close#2

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ from sys import argv, exit
 import requests
 
 def main():
-	try: 
+    try:
         print(argv)
         api_key = argv[1]
     except IndexError:
         print("Please give correct API key")
         exit()
 
+    print("\nWhat city, country?")
+    print("\nUse the format: city, country")
+    location = input("city, country: > ")
+    
+    print("\nHow many days do you want a forecast for?")
+    print("The default is 'current'.")
+    count = int(input("\n: > "))
+
     payload = {"appid": api_key,
-                "q": location,
-                "units": "imperial",}
+                "q": location, #city, country
+                "units": "imperial",
+                "cnt": count,}
+    r = requests.get("http://api.openweathermap.org/data/2.5/weather?", params = payload)
 
-    r = requests.get(" ",params = payload)
-
+#print off ...attributes
     weather = r.json()
-    print("Current Temp: {}".format(weather["main"]["temp"]))
+    print("\nCurrent Temp: {}".format(weather["main"]["temp"]))
+
 
 
 if __name__ == '__main__':
-	main()
+    main()
