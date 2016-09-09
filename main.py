2e619094cba02148054fc0e1469a3cba7d44883e
lava m <lavanymkanna@gmail.com>
2016-09-09 12:57:48 -0400

    added little in main function

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ from sys import argv, exit
 import requests
 
 def main():
-	pass
+	try: 
+        print(argv)
+        api_key = argv[1]
+    except IndexError:
+        print("Please give correct API key")
+        exit()
 
+    payload = {"appid": api_key,
+                "q": location,
+                "units": "imperial",}
+
+    r = requests.get(" ",params = payload)
+
+    weather = r.json()
+    print("Current Temp: {}".format(weather["main"]["temp"]))
 
 
 if __name__ == '__main__':
