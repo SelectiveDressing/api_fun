83efd20185724234548330aeb7e90a37d2b47421
billbunkum <william.richard.hurst@gmail.com>
2016-09-09 15:32:30 -0400

    working on 10-day forecast; need to convert hex objects into readable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sys import argv, exit
+from os import sys
 import requests
 
 def main():
@@ -13,21 +14,44 @@ def main():
     print("\nUse the format: city, country")
     location = input("city, country: > ")
     
-    print("\nHow many days do you want a forecast for?")
-    print("The default is 'current'.")
-    count = int(input("\n: > "))
+    print("0: exit")
+    print("1: Daily Weather")
+    print("2: 5-day Forecast")
+    print("3: 10-day Forecast")
+
+    choice = input("\n: > ")
 
     payload = {"appid": api_key,
                 "q": location, #city, country
-                "units": "imperial",
-                "cnt": count,}
-    r = requests.get("http://api.openweathermap.org/data/2.5/weather?", params = payload)
+                "units": "imperial",}
 
-#print off ...attributes
-    weather = r.json()
-    print("\nCurrent Temp: {}".format(weather["main"]["temp"]))
+    if choice == "1":
+        r = requests.get("http://api.openweathermap.org/data/2.5/weather?", params = payload)
+        weather = r.json()
+        print(weather)
+#5-day
+    elif choice == "2":
+        r = requests.get("http://api.openweathermap.org/data/2.5/forecast?", params = payload)
+        weather = r.json()
+        
+#        for times in weather.items():
+#            if "weather" in times:
+#                print("\nCurrent Temp: {}".format(times))
 
-#booty
+#10 day
+    elif choice == "3":
+        payload["cnt"] = 10
+        r = requests.get("http://api.openweathermap.org/data/2.5/forecast/daily?", params = payload)   
+        weather = r.json()
+
+        emptyList = []
+        for item in weather:
+            emptyList.append(filter(lambda x: x["temp"], item))
+        print(emptyList)
+
+    else:
+        sys.exit()
+
 
 if __name__ == '__main__':
     main()
