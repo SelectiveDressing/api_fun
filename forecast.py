9598206c5d822b00b98aa1ef5a64fe888c41b57f
lava m <lavanymkanna@gmail.com>
2016-09-09 16:49:47 -0400

    classes add

---
+++ forecast.py
@@ -0,0 +1,39 @@
+class Forecast():
+    def daily_weather(self,weather):
+        pass
+    def five_day_weather(self,weather):
+        pass
+    def ten_day_weather(self,weather):
+        emptyList = []
+        for item in weather.keys():
+            if item == "list":
+                emptyList.append(weather["list"])
+        #print(emptyList)
+        raw_temp = []
+        raw_humidity = []
+        raw_pressure = []
+        raw_weather = []
+        for x_list in emptyList:
+             for dict_item in x_list:
+                if "temp" in dict_item:
+                    raw_temp.append(dict_item["temp"])
+                    #print(raw_temp)
+
+                if "humidity" in dict_item:
+                    raw_humidity.append(dict_item["humidity"])
+                    #print(raw_humidity)
+
+                if "pressure" in dict_item:
+                    raw_pressure.append(dict_item["pressure"])
+                    #print(raw_pressure)
+
+                if "weather" in dict_item:
+                    raw_weather.append(dict_item["weather"])
+                    #print(raw_weather)
+        print(raw_temp)
+        print("*"*10)
+        print(raw_humidity)
+        print("*"*10)
+        print(raw_pressure)
+        print("*"*10)
+        print(raw_weather)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from sys import argv, exit
 from os import sys
 import requests
-
+from forecast import Forecast
+user_forecast = Forecast()
 def main():
     try:
         print(argv)
@@ -28,11 +29,12 @@ def main():
     if choice == "1":
         r = requests.get("http://api.openweathermap.org/data/2.5/weather?", params = payload)
         weather = r.json()
-        print(weather)
+        user_forecast.daily_weather(weather)
 #5-day
     elif choice == "2":
         r = requests.get("http://api.openweathermap.org/data/2.5/forecast?", params = payload)
         weather = r.json()
+        user_forecast.five_day_weather(weather)
         
 #        for times in weather.items():
 #            if "weather" in times:
@@ -43,40 +45,43 @@ def main():
         payload["cnt"] = 10
         r = requests.get("http://api.openweathermap.org/data/2.5/forecast/daily?", params = payload)   
         weather = r.json()
+        user_forecast.ten_day_weather(weather)
 
-        emptyList = []
-        for item in weather.keys():
-            if item == "list":
-                emptyList.append(weather["list"])
-        #print(emptyList)
-        raw_temp = []
-        raw_humidity = []
-        raw_pressure = []
-        raw_weather = []
-        for x_list in emptyList:
-             for dict_item in x_list:
-                if "temp" in dict_item:
-                    raw_temp.append(dict_item["temp"])
-                    #print(raw_temp)
 
-                if "humidity" in dict_item:
-                    raw_humidity.append(dict_item["humidity"])
-                    #print(raw_humidity)
 
-                if "pressure" in dict_item:
-                    raw_pressure.append(dict_item["pressure"])
-                    #print(raw_pressure)
+        # emptyList = []
+        # for item in weather.keys():
+        #     if item == "list":
+        #         emptyList.append(weather["list"])
+        # #print(emptyList)
+        # raw_temp = []
+        # raw_humidity = []
+        # raw_pressure = []
+        # raw_weather = []
+        # for x_list in emptyList:
+        #      for dict_item in x_list:
+        #         if "temp" in dict_item:
+        #             raw_temp.append(dict_item["temp"])
+        #             #print(raw_temp)
 
-                if "weather" in dict_item:
-                    raw_weather.append(dict_item["weather"])
-                    #print(raw_weather)
-        print(raw_temp)
-        print("*"*10)
-        print(raw_humidity)
-        print("*"*10)
-        print(raw_pressure)
-        print("*"*10)
-        print(raw_weather)
+        #         if "humidity" in dict_item:
+        #             raw_humidity.append(dict_item["humidity"])
+        #             #print(raw_humidity)
+
+        #         if "pressure" in dict_item:
+        #             raw_pressure.append(dict_item["pressure"])
+        #             #print(raw_pressure)
+
+        #         if "weather" in dict_item:
+        #             raw_weather.append(dict_item["weather"])
+        #             #print(raw_weather)
+        # print(raw_temp)
+        # print("*"*10)
+        # print(raw_humidity)
+        # print("*"*10)
+        # print(raw_pressure)
+        # print("*"*10)
+        # print(raw_weather)
     else:
         sys.exit()
 
